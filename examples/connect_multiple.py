f42c21db89f985fd436253156b16982991e800be
Ville Kärkkäinen <ville.karkkainen@nordhero.com>
2022-11-16 14:43:02 +0200

    Separate example code for connecting multi/single CSs

---
+++ examples/connect_multiple.py
@@ -0,0 +1,16 @@
+import asyncio
+from charging_station import connect
+
+from ocpp_asgi.router import Subprotocol
+
+
+async def main():
+    await asyncio.gather(
+        connect(charging_station_id="1", subprotocol=Subprotocol.ocpp16),
+        connect(charging_station_id="2", subprotocol=Subprotocol.ocpp201),
+        connect(charging_station_id="3", subprotocol=Subprotocol.ocpp201),
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ examples/connect_single.py
@@ -0,0 +1,14 @@
+import asyncio
+from charging_station import connect
+
+from ocpp_asgi.router import Subprotocol
+
+
+async def main():
+    await asyncio.gather(
+        connect(charging_station_id="4", subprotocol=Subprotocol.ocpp16),
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
